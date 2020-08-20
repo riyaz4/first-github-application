d1fa12399b412b23761a2f806ccec20cfdec77e0
riyaz4 <riyaz.bkc@gmail.com>
2020-08-20 17:42:26 +0530

    First Github project

--- app/src/main/java/com/ws/firstgithubapp/MainActivity2.java
+++ app/src/main/java/com/ws/firstgithubapp/MainActivity2.java
@@ -13,5 +13,6 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         TextView t = findViewById(R.id.text);
+        t.setText("Riyaz");
     }
 }
