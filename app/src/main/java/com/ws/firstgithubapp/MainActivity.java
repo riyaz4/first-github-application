51ced0cee1cd50107827d8f498e46fef3627a9fe
riyaz4 <riyaz.bkc@gmail.com>
2020-08-20 17:18:59 +0530

    First Github project

--- app/src/main/java/com/ws/firstgithubapp/MainActivity.java
+++ app/src/main/java/com/ws/firstgithubapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.ws.firstgithubapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text);
     }
 }
